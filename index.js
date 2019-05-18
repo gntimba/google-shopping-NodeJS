0e22f22ac273871d75864b102b4ebed35e1bee38
godfrey Timba <gntimba@gmail.com>
2019-05-18 15:26:20 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const { google } = require('googleapis')
+const path = require('path');
+const serviceAccountJwt = require('./Shopping API-145f77376c3c.json')
+const app = express()
+
+app.get('/', function (req, res) {
+  async function runSample() {
+    const client = await google.auth.getClient({
+      keyFile: path.join(__dirname, 'Shopping API-145f77376c3c.json'),
+      scopes: 'https://www.googleapis.com/auth/content',
+    });
+
+    const params = {
+      merchantId: 134645521
+    };
+    const content = google.content({ version: 'v2.1', auth: client });
+    const response = await content.products.list(params)
+    console.log(response.data);
+    res.json(response.data)
+
+    return response.data;
+  }
+
+  runSample().catch(console.error)
+})
+
+app.get('/insert', function (req, res) {
+  async function insert() {
+    const client = await google.auth.getClient({
+      keyFile: path.join(__dirname, 'Shopping API-145f77376c3c.json'),
+      scopes: 'https://www.googleapis.com/auth/content',
+    });
+
+    const params = {
+      merchantId: "134645521",
+      resource: {
+        channel: "online",
+        contentLanguage: "en",
+        offerId: "g003",
+        targetCountry: "ZA",
+        brand: "samsung",
+        title: "Samsung A30",
+        link: "https://www.samsung.com/za/smartphones/galaxy-a30-a305/SM-A305FZBEXFA/",
+        description:"The best in samsung",
+        imageLink: "https://i.gadgets360cdn.com/large/samsung_galaxy_a30_a50_fronts_1551359465736.jpg",
+        price: {
+          currency: "ZAR",
+          value: "4499"
+        },
+        availability: "in stock"
+      }
+    };
+    const content = google.content({ version: 'v2.1', auth: client });
+    const response = await content.products.insert(params)
+    console.log(response.data);
+    res.json(response.data)
+
+    return response.data;
+  }
+
+  insert().catch(console.error)
+})
+
+
+
+app.listen(3000)
